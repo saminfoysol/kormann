4d2a7cc77e1ad481feaf4b98aa78f26889441789
saf243 <saf243@cornell.edu>
2017-12-30 13:05:44 -0500

    flicker

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
 
 				if ($(window).width() < 600){
 					return;
@@ -15,4 +15,39 @@ $(document).ready(function () {
                     $(".home-hero").css('background-image', imageFile[currentIndex++]).fadeIn(3000);
                 }, 9000);
                 }   
-            });
+            });*/
+
+function imagesPreload(){
+     var imgArray = new Array("img/background3.jpg", "img/background16.jpg", "img/background19.jpg","img/background24.jpg","img/background21.jpg","img/background14.jpg");
+     for (var i=0; i<imgArray.length; i++) {
+         (new Image()).src = imgArray[i];
+     }
+ }
+
+ window.onload = function() {
+    document.getElementById("first").onmouseover = function fun() {
+        $('.home-hero').css('background-image','url(img/background3.jpg)').fadeIn(3000);
+        //validation code to see State field is mandatory.  
+    }
+    document.getElementById("second").onmouseover = function fun() {
+        $('.home-hero').css('background-image','url(img/background16.jpg)').fadeIn(3000);
+        //validation code to see State field is mandatory.  
+    }
+    document.getElementById("third").onmouseover = function fun() {
+        $('.home-hero').css('background-image','url(img/background19.jpg)').fadeIn(3000);
+        //validation code to see State field is mandatory.  
+    }
+    document.getElementById("fourth").onmouseover = function fun() {
+        $('.home-hero').css('background-image','url(img/background24.jpg)').fadeIn(3000);
+        //validation code to see State field is mandatory.  
+    }
+    document.getElementById("fifth").onmouseover = function fun() {
+        $('.home-hero').css('background-image','url(img/background21.jpg)').fadeIn(3000);
+        //validation code to see State field is mandatory.  
+    }
+    document.getElementById("sixth").onmouseover = function fun() {
+        $('.home-hero').css('background-image','url(img/background14.jpg)').fadeIn(3000);
+        //validation code to see State field is mandatory.  
+    }
+
+}
